d20f1419000e5ae21cf5db82e7e46ed5774c5c99
NV Nhận <nv.bigbang@gmail.com>
2020-05-03 00:14:30 +0700

    Done

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -12,13 +12,22 @@ class HomeController extends Controller
     {
         $q = $request->q;
         $sort = $request->sort;
+        $data = [];
 
-        return dd(self::get_shopee_data($q, $sort));
+        if (!empty($q)) {
+            $data = array_merge(
+                self::get_nguyen_kim_data($q, $sort),
+                self::get_shopee_data($q, $sort),
+                self::get_sendo_data($q, $sort)
+            );
+            if (empty($sort)) shuffle($data);
+            else if ($sort == 'price-asc') usort($data, function ($a, $b) { return $a->price > $b->price; });
+        }
 
-        return view('home');
+        return view('home', compact('data'));
     }
 
-    public function get_nguyen_kim_data(string $q, string $sort)
+    public function get_nguyen_kim_data(string $q, $sort = '')
     {
         $nguyenkim_url = "https://www.nguyenkim.com/tim-kiem.html?&q=$q&sort_by=relevance&sort_order=asc";
         if ($sort == 'price-asc')
@@ -30,26 +39,29 @@ class HomeController extends Controller
         $content = file_get_html($nguyenkim_url);
         $doms = $content->find('[id=pagination_contents] [class$=nk-new-layout-product-grid]');
         $nguyenkim_parse_data = [];
+        $cnt = 0;
         foreach ($doms as $key => $value) {
             $tmp = new stdClass;
-            $tmp->source = 'nk';
+            $tmp->source = 'nguyenkim';
             $tmp->link = $value->find('a', 0)->href;
             $tmp->title = $value->find('p', 0)->plaintext;
 
             $data = $value->find('input', 0);
             $tmp->image = $data->getAttribute('data-imgurl');
             $tmp->price = $data->getAttribute('data-price');
-            $tmp->description = $data->getAttribute('data-description');
+            // $tmp->description = $data->getAttribute('data-description');
             $tmp->quantity = $data->getAttribute('data-quantity');
             $tmp->brand = $data->getAttribute('data-brand');
-            $tmp->category = $data->getAttribute('data-category');
+            // $tmp->category = $data->getAttribute('data-category');
 
             $nguyenkim_parse_data[] = $tmp;
+            $cnt++;
+            if ($cnt == 20) break;
         }
         return $nguyenkim_parse_data;
     }
 
-    public function get_shopee_data(string $q, string $sort)
+    public function get_shopee_data(string $q, $sort = '')
     {
         $shopee_url = "https://shopee.vn/api/v2/search_items/?keyword=$q";
         if ($sort == 'price-asc')
@@ -69,23 +81,66 @@ class HomeController extends Controller
         $output = curl_exec($ch);
 
         $data = [];
+        $cnt = 0;
         // handle error; error output
         if (curl_getinfo($ch, CURLINFO_HTTP_CODE) == 200) {
             $output = json_decode($output);
             foreach ($output->items as $key => $value) {
                 $tmp = new stdClass;
-                $tmp->source = 'sp';
+                $tmp->source = 'shopee';
                 $tmp->link = "https://shopee.vn/nvnhận-i.$value->shopid.$value->itemid";
                 $tmp->title = $value->name;
 
                 $tmp->image = "https://cf.shopee.vn/file/" . $value->image;
                 $tmp->price = $value->price_min / 1e5;
-                $tmp->description = "";
                 $tmp->quantity = $value->stock;
                 $tmp->brand = $value->brand;
-                $tmp->category = "";
 
                 $data[] = $tmp;
+                $cnt++;
+                if ($cnt == 20) break;
+            }
+        }
+
+        curl_close($ch);
+        return $data;
+    }
+
+    public function get_sendo_data(string $q, $sort = '')
+    {
+        $sendo_url = "https://www.sendo.vn/m/wap_v2/search/product?q=$q&sortType=rank";
+        if ($sort == 'price-asc')
+            $sendo_url = "https://www.sendo.vn/m/wap_v2/search/product?q=$q&sortType=price_asc";
+        else if ($sort == 'price-desc')
+            $sendo_url = "https://www.sendo.vn/m/wap_v2/search/product?q=$q&sortType=price_desc";
+
+        // make request
+        $ch = curl_init();
+        curl_setopt($ch, CURLOPT_URL, $sendo_url);
+        curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1);
+        curl_setopt($ch, CURLOPT_USERAGENT, 'Mozilla/4.0 (compatible; MSIE 6.0; Windows NT 5.1; SV1; .NET CLR 1.0.3705; .NET CLR 1.1.4322)');
+
+        $output = curl_exec($ch);
+
+        $data = [];
+        $cnt = 0;
+        // handle error; error output
+        if (curl_getinfo($ch, CURLINFO_HTTP_CODE) == 200) {
+            $output = json_decode($output);
+            foreach ($output->result->data as $key => $value) {
+                $tmp = new stdClass;
+                $tmp->source = 'sendo';
+                $tmp->link = "https://www.sendo.vn/$value->cat_path";
+                $tmp->title = $value->name;
+
+                $tmp->image = $value->image;
+                $tmp->price = $value->price;
+                $tmp->quantity = 99; //$value->stock;
+                $tmp->brand = ""; //$value->brand;
+
+                $data[] = $tmp;
+                $cnt++;
+                if ($cnt == 20) break;
             }
         }
 
--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -1,28 +1,86 @@
 <!DOCTYPE html>
 <html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <title>Web so sánh</title>
-    <link rel="stylesheet" href="/css/app.css" />
-</head>
-<body>
-    <div class="container">
-        <div class="search-box">
-            <form action="#" method="GET">
-                <input name="q" placeholder="Nhập từ khóa..." />
-                <select name="sort" id="sort">
-                    <option value="">Mặc định</option>
-                    <option value="price-asc">Giá tăng dần</option>
-                    <option value="price-desc">Giá giảm dần</option>
-                </select>
-                <button type="submit">Tìm</button>
-            </form>
+    <head>
+        <meta charset="UTF-8" />
+        <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+        <meta http-equiv="X-UA-Compatible" content="ie=edge" />
+        <title>Web so sánh</title>
+        <link
+            rel="stylesheet"
+            href="https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/css/bootstrap.min.css"
+            integrity="sha384-Vkoo8x4CGsO3+Hhxv8T/Q5PaXtkKtu6ug5TOeNV6gBiFeWPGFN9MuhOf23Q9Ifjh"
+            crossorigin="anonymous"
+        />
+        <link rel="stylesheet" href="/css/app.css" />
+    </head>
+    <body>
+        <div class="container">
+            <div class="search-box">
+                <h1 style="text-align: center;">WEB SO SÁNH</h1>
+                <form action="#" method="GET">
+                    <div class="row">
+                        <div class="col-6">
+                            <input
+                                class="form-control"
+                                name="q"
+                                placeholder="Nhập từ khóa..."
+                            />
+                        </div>
+                        <div class="col-3">
+                            <select class="form-control" name="sort" id="sort">
+                                <option value="">Mặc định</option>
+                                <option value="price-asc">Giá tăng dần</option>
+                                <option value="price-desc">Giá giảm dần</option>
+                            </select>
+                        </div>
+                        <div class="col-3">
+                            <button class="btn btn-primary btn-block" type="submit">
+                                Tìm
+                            </button>
+                        </div>
+                    </div>
+                </form>
+            </div>
+            <div class="content">
+                <div class="row">
+                    @foreach ($data as $item)
+                    <div class="col-3 {{$item->source}}">
+                        <div class="card">
+                            <img src="{{$item->image}}" class="card-img-top" />
+                            <div class="card-body">
+                                <h5 class="card-title">{{$item->title}}</h5>
+                                <p class="card-text">
+                                    {{number_format($item->price)}}đ
+                                </p>
+                                <a
+                                    href="{{$item->link}}"
+                                    target="blank"
+                                    class="btn btn-primary"
+                                    >Chi tiết >></a
+                                ><br />
+                                <small>{{$item->source}}</small>
+                            </div>
+                        </div>
+                    </div>
+                    @endforeach
+                </div>
+            </div>
         </div>
-        <div class="content">
 
-        </div>
-    </div>
-</body>
+        <script
+            src="https://code.jquery.com/jquery-3.4.1.slim.min.js"
+            integrity="sha384-J6qa4849blE2+poT4WnyKhv5vZF5SrPo0iEjwBvKU7imGFAV0wwj1yYfoRSJoZ+n"
+            crossorigin="anonymous"
+        ></script>
+        <script
+            src="https://cdn.jsdelivr.net/npm/popper.js@1.16.0/dist/umd/popper.min.js"
+            integrity="sha384-Q6E9RHvbIyZFJoft+2mJbHaEWldlvI9IOYy5n3zV9zzTtmI3UksdQRVvoxMfooAo"
+            crossorigin="anonymous"
+        ></script>
+        <script
+            src="https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/js/bootstrap.min.js"
+            integrity="sha384-wfSDF2E50Y2D1uUdj0O3uMBJnjuUD4Ih7YwaYd1iqfktj0Uod8GCExl3Og8ifwB6"
+            crossorigin="anonymous"
+        ></script>
+    </body>
 </html>
